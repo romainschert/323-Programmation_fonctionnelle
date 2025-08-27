f9e4e4e70ac25a1b0c9443d6003744ef4fef2cb2
romainschert <150144520+romainschert@users.noreply.github.com>
2025-08-27 11:41:19 +0200

    partie 1ex marché

---
+++ personnel/Exercice-Marché_RomainSchertenleib/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_Marché_RomainSchertenleib
+{
+    public class Product
+    {
+        public int emplacement { get; set; }
+        public string Producteur { get; set; }
+        public string Produit { get; set; }
+        public int Quantité { get; set; }
+        public int Unité { get; set; }
+        public double prixparunité { get; set; }
+    }
+}
---
+++ personnel/Exercice-Marché_RomainSchertenleib/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercice_Marché_RomainSchertenleib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
